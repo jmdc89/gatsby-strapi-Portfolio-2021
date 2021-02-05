32bbe54d2a050bbf69189a8995249a10f2884bb5
Jose Manuel <jmdcarq89@gmail.com>
2021-02-05 17:35:54 +0100

    Social Links update

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -3,30 +3,30 @@ import {
   FaFacebookMessenger,
   FaLinkedinIn,
   FaBehance,
-  FaTwitter,
+  FaGithub,
 } from "react-icons/fa"
 
 const data = [
   {
     id: 1,
     icon: <FaFacebookMessenger className="social-icon"></FaFacebookMessenger>,
-    url: "https://www.twitter.com",
+    url: "https://www.facebook.com/josemanuel.dominguez.710/",
   },
   {
     id: 2,
     icon: <FaLinkedinIn className="social-icon"></FaLinkedinIn>,
-    url: "https://www.twitter.com",
+    url: "https://www.linkedin.com/in/jos%C3%A9-manuel-dom%C3%ADnguez-425b1b113/",
   },
   {
     id: 4,
-    icon: <FaBehance className="social-icon"></FaBehance>,
-    url: "https://www.twitter.com",
+    icon: <FaGithub className="social-icon"></FaGithub>,
+    url: "https://github.com/jmdc89",
   },
-  {
-    id: 5,
-    icon: <FaTwitter className="social-icon"></FaTwitter>,
-    url: "https://www.twitter.com",
-  },
+  // {
+  //   id: 5,
+  //   icon: <FaTwitter className="social-icon"></FaTwitter>,
+  //   url: "https://www.twitter.com",
+  // },
 ]
 const links = data.map(link => {
   return (
